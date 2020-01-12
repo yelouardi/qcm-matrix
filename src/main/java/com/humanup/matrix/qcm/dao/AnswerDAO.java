5bf01708bb95ebb4536fa81a4a23339ec92925c6
khalil0M <khalil.kouiss@gmail.com>
2020-01-12 23:35:35 +0100

    [QCM-MATRIX] add methodes for search by question id

--- src/main/java/com/humanup/matrix/qcm/dao/AnswerDAO.java
+++ src/main/java/com/humanup/matrix/qcm/dao/AnswerDAO.java
@@ -3,9 +3,11 @@ package com.humanup.matrix.qcm.dao;
 import com.humanup.matrix.qcm.dao.entities.Answer;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AnswerDAO extends CrudRepository<Answer, Long> {
 
     @Query("SELECT a FROM Answer a WHERE lower(a.emailPerson) = :emailPerson and a.choice.choiceId = :choiceId ")
