e8a84d19bc8e6983455efedf54c07fac86ff13f0
ladib khalid <5874428-khalidLdev@users.noreply.gitlab.com>
2020-05-10 20:04:15 +0200

    add exceptions to interfaces

--- src/main/java/com/humanup/matrix/qcm/bs/QuestionBS.java
+++ src/main/java/com/humanup/matrix/qcm/bs/QuestionBS.java
@@ -1,11 +1,12 @@
 package com.humanup.matrix.qcm.bs;
 
+import com.humanup.matrix.qcm.aop.dto.QuestionException;
 import com.humanup.matrix.qcm.vo.QuestionVO;
 
 import java.util.List;
 
 public interface QuestionBS {
-    boolean createQuestion(QuestionVO question);
+    boolean createQuestion(QuestionVO question) throws QuestionException;
     List<QuestionVO> findListQuestion();
     QuestionVO findQuestionById(Long questionId);
     QuestionVO findQuestionByQuestionText(String questionText);
