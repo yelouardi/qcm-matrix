ae5231c41578410daf966746f3a555474ca9c14c
ladib khalid <5874428-khalidLdev@users.noreply.gitlab.com>
2020-05-10 19:44:41 +0200

    add exceptions

---
+++ src/main/java/com/humanup/matrix/qcm/aop/LoggingAspect.java
@@ -0,0 +1,40 @@
+package com.humanup.matrix.qcm.aop;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class LoggingAspect
+{
+    private static final Logger LOGGER = LogManager.getLogger(LoggingAspect.class);
+
+    @Around("execution(* com.humanup.matrix.qcm.bs..*(..)) || execution(* com.humanup.matrix.qcm.dao..*(..))")
+    public Object profileAllMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable
+    {
+            MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+            //Get intercepted method details
+            String className = methodSignature.getDeclaringType().getSimpleName();
+            String methodName = methodSignature.getName();
+
+            final StopWatch stopWatch = new StopWatch();
+
+            //Measure method execution time
+            stopWatch.start();
+            Object result = proceedingJoinPoint.proceed();
+            stopWatch.stop();
+
+            //Log method execution time
+            LOGGER.info("Execution time of " + className + "." + methodName + " "
+                    + ":: " + stopWatch.getTotalTimeMillis() + " ms");
+
+            return result;
+    }
+}
---
+++ src/main/java/com/humanup/matrix/qcm/aop/dto/AnswerException.java
@@ -0,0 +1,16 @@
+package com.humanup.matrix.qcm.aop.dto;
+
+public class AnswerException extends HttpException {
+    public AnswerException(String message) {
+        super(message);
+    }
+
+    public AnswerException() {
+        super();
+    }
+
+    @Override
+    public String getMessage() {
+        return "Cannot Create Answer";
+    }
+}
